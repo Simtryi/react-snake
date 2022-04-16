614f698697b729e000a4651e4b8806a681fb5c19
simple <2579115705@qq.com>
2022-04-16 14:11:36 +0800

    feat: add app.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,38 @@
-import { FC } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux'
 import { store } from './store'
-import Counter from './components/Counter'
+import './App.less'
 
-const App: FC = () => {
-    return (
-        <Provider store={store}>
-            <Counter />
-        </Provider>
-    )
+interface IProps {}
+
+interface IState {}
+
+export default class App extends React.Component<IProps, IState> {
+
+    //  处理键盘事件
+    handleKeyDown = (event: any) => {
+        const keyCode = event.nativeEvent.keyCode;
+        console.log("key code: ", keyCode)
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+                <div className="app" onKeyDown={this.handleKeyDown} tabIndex={0}>
+                    <div className="layout">
+                        <div className="layout-top">
+                            <div className="content">
+                                <div className="main">
+                                    <div className="screen">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Provider>
+        )
+    }
+
 }
-
-export default App
