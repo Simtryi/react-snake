fd8d2fd0f547dff059a6fcddf7caa0817455d56e
simple <2579115705@qq.com>
2022-04-16 15:06:07 +0800

    feat: add number.

--- src/app/components/info/index.tsx
+++ src/app/components/info/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Number from "../number";
 const snakeImg = require('../../resources/images/snack.png')
 const robotImg = require('../../resources/images/robot.png')
 
@@ -13,19 +14,23 @@ export default class Info extends React.Component<IProps, IState> {
             <div className="right">
                 <div className='record'>
                     <label>分数：</label>
+                    <Number />
                     <img src={snakeImg} />
                 </div>
 
                 <div className='level'>
                     <label>模式</label>
+                    <Number />
                 </div>
 
                 <div className='set'>
-
+                    <Number />
+                    <Number />
                 </div>
 
                 <div className='time'>
                     <img src={robotImg} />
+                    <Number />
                 </div>
             </div>
         )
---
+++ src/app/components/number/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './index.less'
+
+interface IProps {}
+
+interface IState {}
+
+export default class Number extends React.Component<IProps, IState> {
+
+    render() {
+        return (
+            <div className="number">
+
+            </div>
+        )
+    }
+
+}
